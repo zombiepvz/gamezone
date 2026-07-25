6db7174c4eb592629208fd395b02bd41f2d4a62a
zombiepvz <giulianipeyton@gmail.com>
2026-07-25 12:50:20 -0400

    ydkj

---
+++ ydkj/project/actions/movefiles.bat
@@ -0,0 +1,37 @@
+set APPATH="C:\Users\benjacobs\Desktop\facebook-mobile\trunk\ydkj-facebook\fla\actionpackages\"
+
+del 1018.swf
+copy %APPATH%DisOrDat.swf 1018.swf
+
+del 1016.swf
+copy %APPATH%Ending.swf 1016.swf
+
+del 1015.swf
+copy %APPATH%Game.swf 1015.swf
+
+del 1019.swf
+copy %APPATH%Gibberish.swf 1019.swf
+
+del 1021.swf
+copy %APPATH%JackAttack.swf 1021.swf
+
+del 1013.swf
+copy %APPATH%LoserWheel.swf 1013.swf
+
+del 1014.swf
+copy %APPATH%Menu.swf 1014.swf
+
+del 1022.swf
+copy %APPATH%Music.swf 1022.swf
+
+del 1017.swf
+copy %APPATH%Opening.swf 1017.swf
+
+del 1020.swf
+copy %APPATH%PlayerList.swf 1020.swf
+
+del 1024.swf
+copy %APPATH%Segue.swf 1024.swf
+
+del 1023.swf
+copy %APPATH%Shortie.swf 1023.swf
